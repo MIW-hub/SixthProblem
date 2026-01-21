f8197b8002558dcdd35917538305849e745aef64
MIW-hub <miws4747@gmail.com>
2026-01-21 11:26:46 +0900

    파일 업로드

--- Source/SixthProblem/Private/SpawnMoveActor.cpp
+++ Source/SixthProblem/Private/SpawnMoveActor.cpp
@@ -1,13 +1,24 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "SpawnMoveActor.h"
+#include <MoveActor.h>
 
 // Sets default values
 ASpawnMoveActor::ASpawnMoveActor()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	CurrentLocation = FVector::ZeroVector;
+	StartLocation = FVector::ZeroVector;
+	MoveDirection = FVector::ForwardVector;
+	ActorRotaion = FRotator::ZeroRotator;
+
+	SceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("SceneRoot"));
+	SetRootComponent(SceneRoot);
+
+	StaticComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+	StaticComp->SetupAttachment(SceneRoot);
 
 }
 
@@ -15,13 +26,46 @@ ASpawnMoveActor::ASpawnMoveActor()
 void ASpawnMoveActor::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	SetLifeSpan(13.0f);
+	RanSpeed = FMath::FRandRange(10.0f, MoveSpeed);
+	RanSpin = FMath::FRandRange(10.0f, RotationSpeed);
+	//SetActorLocation(FVector(0.0f, 0.0f, 60.0f)); // - x,y,z
+	SetActorRotation(FRotator(0.0f, 0.0f, 0.0f)); // - pitch,yaw,roll
+	SetActorScale3D(FVector(1.0f, 1.0f, 0.2f));
 }
 
 // Called every frame
 void ASpawnMoveActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	SpinActorA(DeltaTime);
+	MoveActorA(DeltaTime);
 
 }
 
+void ASpawnMoveActor::SetSpeed(float InSpeed)
+{
+	MoveSpeed = InSpeed;
+}
+
+void ASpawnMoveActor::SetSpin(float InSpin)
+{
+	RotationSpeed = InSpin;
+}
+
+void ASpawnMoveActor::MoveActorA(float DeltaTime)
+{
+
+	if (!FMath::IsNearlyZero(MoveSpeed))
+	{
+		AddActorLocalOffset(FVector(0.0f, 0.0f, DeltaTime * RanSpeed * MoveSpeed));
+	}
+}
+void ASpawnMoveActor::SpinActorA(float DeltaTime)
+{
+
+	if (!FMath::IsNearlyZero(RotationSpeed))
+	{
+		AddActorLocalRotation(FRotator(0.0f, DeltaTime * RanSpin * RotationSpeed, 0.0f));
+	}
+}
--- Source/SixthProblem/Public/SpawnMoveActor.h
+++ Source/SixthProblem/Public/SpawnMoveActor.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -18,9 +18,36 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spawn");
+	USceneComponent* SceneRoot;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spawn");
+	UStaticMeshComponent* StaticComp;
 
-public:	
+private:
 	// Called every frame
-	virtual void Tick(float DeltaTime) override;
 
+public:
+	FVector StartLocation;
+	FVector CurrentLocation; // 처음 시작 위치 저장용
+	FVector MoveDirection; //
+	FRotator ActorRotaion;
+	float TargetLocation = 0.0f; // 목표 지점
+	bool Returning = false; // 현재 돌아오는 중인지 체크
+
+	void SpinActorA(float DeltaTime);
+	void SetSpin(float InSpin);
+	void SetSpeed(float InSetSpeed);
+
+	float GetSpeed() const { return MoveSpeed; }
+	float GetSpin()  const { return RotationSpeed; }
+
+	void MoveActorA(float DeltaTime);
+	virtual void Tick(float DeltaTime) override;
+	float RanSpeed;
+	float RanSpin;
+
+	float MoveSpeed;
+	float RotationSpeed;
+
+	
 };
